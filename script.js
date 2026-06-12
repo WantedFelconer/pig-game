d6d1a49bc524e79d98a76460c1ee18e908687ddb
wantedfelconer <emon62224@gmail.com>
2026-06-12 23:05:24 +0600

    beta

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 'use strict';
 
-let rollDice = function () {
+let rollDiceNum = function () {
   return Math.trunc(Math.random() * 6) + 1;
 };
+let diceNum = 0;
+let score = [0, 0];
+let playerNo = 0;
+let currentScore = 0;
 
 let btnNew = document.querySelector('.btn--new');
 let btnRoll = document.querySelector('.btn--roll');
 let btnHold = document.querySelector('.btn--hold');
 let diceImg = document.querySelector('.dice');
-let p1CurrentScore = document.getElementById('current--0');
-let p2CurrentScore = document.getElementById('current--1');
-
-let diceNum = 0;
-let score = [0, 0];
-let playerNo = 0;
+document.getElementById(`score--0`).textContent = 0;
+document.getElementById(`score--1`).textContent = 0;
 
 let switchPlayer = function () {
   playerNo = 1 - playerNo;
 };
 
+let isWin = function () {};
+
+let newGame = function () {
+  let diceNum = 0;
+  let score = [0, 0];
+  let playerNo = 0;
+  let currentScore = 0;
+  document.getElementById(`current--${0}`).textContent = currentScore;
+  document.getElementById(`current--${1}`).textContent = currentScore;
+  document.getElementById(`score--${0}`).textContent = score[0];
+  document.getElementById(`score--${1}`).textContent = score[1];
+};
+
 btnRoll.addEventListener('click', function () {
-  diceNum = rollDice();
-  score[playerNo] += diceNum;
-  console.log(score[playerNo]);
+  diceNum = rollDiceNum();
   diceImg.src = `dice-${diceNum}.png`;
-  p1CurrentScore.textContent = diceNum;
+  if (diceNum == 1) {
+    currentScore = 0;
+    document.getElementById(`current--${playerNo}`).textContent = currentScore;
+    switchPlayer();
+    return;
+  }
+  currentScore += diceNum;
+  document.getElementById(`current--${playerNo}`).textContent = currentScore;
+});
+
+btnHold.addEventListener('click', function () {
+  score[playerNo] += currentScore;
+  document.getElementById(`score--${playerNo}`).textContent = score[playerNo];
+  currentScore = 0;
+  document.getElementById(`current--${playerNo}`).textContent = currentScore;
+  if (score[playerNo] >= 100) {
+    prompt(`Player ${playerNo} won the game`);
+    newGame();
+    return;
+  }
+  switchPlayer();
 });
